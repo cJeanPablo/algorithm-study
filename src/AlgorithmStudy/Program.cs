492685045ea8bdd86881ccbee5783d259f976b56
Jean Pablo Carvalho da Silva <jpablo1026@hotmail.com>
2024-02-03 14:10:28 -0300

    fizz-buzz development

---
+++ src/AlgorithmStudy/Program.cs
@@ -0,0 +1,24 @@
+﻿//Fizz and Buzz refer to any number that's a multiple of 3 and 5 respectively
+
+int[] variablesIn = [1,2,3,4,5,6,7,8,9,10];
+
+
+foreach (int variable in variablesIn)
+{
+    if (variable % 15 == 0)
+    {
+        Console.WriteLine($"Variable {variable} is a fizz buzz");
+    }
+    else if (variable % 3 == 0)
+    {
+        Console.WriteLine($"Variable {variable} is a fizz");
+    }
+    else if ( variable % 5 == 0)
+    {
+        Console.WriteLine($"Variable {variable} is a buzz");
+    }
+    else
+    {
+        Console.WriteLine($"Variable {variable} isnt a fizz buzz");
+    }
+}
